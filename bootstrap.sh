2810aef0e4d1a44682abecd03538e9cd255a1846
Cameron Spear <cameronspear@gmail.com>
2015-07-16 02:24:49 -0700

    Cleanup

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,35 +1,33 @@
+#!/usr/bin/env bash
+
 hash zsh 2>/dev/null || {
-	echo >&2 "Zsh is not installed. Aborting."
-	exit 1
+    echo >&2 "Zsh is not installed. Aborting."
+    exit 1
 }
 
 if [ ! -d ~/.homesick/repos/homeshick ]; then
-	echo "Installing Homeshick."
-	git clone https://github.com/andsens/homeshick.git ~/.homesick/repos/homeshick
-	source ~/.homesick/repos/homeshick/homeshick.sh
-	homeshick link homeshick
+    echo "Installing Homeshick."
+    git clone https://github.com/andsens/homeshick.git ~/.homesick/repos/homeshick
 fi
 
+source ~/.homesick/repos/homeshick/homeshick.sh
+homeshick link homeshick
+
 if [ ! -d ~/.homesick/repos/prezto ]; then
-	echo "Installing Prezto."
-	homeshick clone sorin-ionescu/prezto
+    echo "Installing Prezto."
+    homeshick clone sorin-ionescu/prezto
 fi
 
 if [ ! -d ~/.homesick/repos/dotfiles ]; then
-	echo "Creating a dotfiles castle to manage user configuration."
-	homeshick generate dotfiles
-fi
-
-if [ ! -d ~/.homesick/repos/homeshick-prezto ]; then
-	echo "Installing the default Prezto and Zsh configuration."
-	homeshick clone CWSpear/dotfiles
+    echo "Installing dotfiles."
+    homeshick clone CWSpear/dotfiles
 fi
 
 ZSH=`which zsh`
 
 if [ $ZSH != $SHELL ]; then
-	echo "Changing shell to Zsh."
-	chsh -s $ZSH
+    echo "Changing shell to Zsh."
+    chsh -s $ZSH
 fi
 
-echo "Prezto and Zsh installed. Open a new terminal window or tab and enjoy!"
+echo "Prezto and Zsh installed. Open a new terminal window or tab and enjoy"
